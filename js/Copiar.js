354415373fffff05796217374905966fe8a3ac91
Lucas Risso Patron <rissopatron@gmail.com>
2022-01-22 11:21:20 -0300

    se cambio el metodo del boton copiar

--- js/Copiar.js
+++ js/Copiar.js
@@ -1,24 +1,18 @@
 function seleccionarTexto() {
-    let desde = msg.selectionStart;
-    let hasta = msg.selectionEnd;
-    let sel = msg.value.substring(desde, hasta);
-  
-    if (sel.length > 0) {// si hay algo seleccionado.
-      return sel;
-    } else { // de lo contrario selecciona todo el texto.
-      return msg.select();
-    }
-  }
-  
-  var btncopiar = document.querySelector("#btn-copy");
-  btncopiar.addEventListener("click", () => {  
-    seleccionarTexto();
-    try {
-      let exito = document.execCommand("copy"); // devuelve true o false.
-      // el mensaje a sacar en consola en caso de éxito o de error.
-      let mensage = exito ? "\xe9xito" : "error";
-    } catch (error) {
-      let err = error;
-    }
-  });
+  let desde = msg.selectionStart;
+  let hasta = msg.selectionEnd;
+  let sel = msg.value.substring(desde, hasta);
 
+  if (sel.length > 0) {// si hay algo seleccionado.
+    navigator.clipboard.writeText(sel.value);
+    return sel;
+  } else { // de lo contrario selecciona todo el texto.
+    navigator.clipboard.writeText(msg.value);
+    return msg.select(); 
+  }
+}
+
+var btncopiar = document.querySelector("#btn-copy");
+btncopiar.addEventListener("click", () => {  
+  seleccionarTexto();
+});
